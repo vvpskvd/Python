493088b74c068ef95dffc7f015419fc50f75ab72
vvpskvd <127900056+vvpskvd@users.noreply.github.com>
2023-06-07 22:19:27 -0500

    Ejercicios en Python (VSC)

---
+++ Tabla de multiplicar.py
@@ -0,0 +1,7 @@
+ini=0
+numero=int(input("Hasta que numero desea multiplicar:"))
+numero_mult=int(input("Que numero desea multiplicar:"))
+while ini <= numero  :
+  print ( numero_mult, "x", ini, "=", numero_mult*ini )
+  ini=ini+1
+print("Tabla de multiplicar del numero", numero_mult)
---
+++ if y else.py
@@ -0,0 +1,5 @@
+temp = (float(input("ingrese la temperatura de hoy:")))
+if  temp<35:
+  print("No esta haciendo calor")
+else:
+  print("Esta haciendo calor")
