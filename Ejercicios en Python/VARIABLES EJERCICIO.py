d61de97424207795f208989315876fc694300a74
Vvpskvd <127900056+vvpskvd@users.noreply.github.com>
2024-05-30 21:21:27 -0500

    Python
    By Vvpskvd

---
+++ Ejercicios en Python/VARIABLES EJERCICIO.py
@@ -0,0 +1,25 @@
+miVariable = "HOLA PYTHON" #Variable con cadena de texto.
+print (miVariable)
+print (miVariable) #Se puede usar multiples veces.
+print (miVariable)
+miVariable2 = 18 #Variable con enteros.
+print (miVariable2)
+
+miVariable = "ADIOS PYTHON" #Se puede asignar nuevo valor, pero el viejo se borra.
+print (miVariable)
+
+#2DA PARTE VARIABLES
+X = 10
+Y = 2
+SUMA = X + Y
+print ("La suma de ", X, " y ", Y, " es ", SUMA) #SUMAR DOS VARIABLES
+print (id(X)) #BUSCAR LA POSICION DE MEMORIA QUE ALMACENA LA VARIABLE
+print (id(Y))
+
+#Ejercicio Variable.
+nombre = "Juan Perez"
+telefono = 55621817
+email = "jperez@mail.com"
+print (nombre)
+print (telefono)
+print (email)
